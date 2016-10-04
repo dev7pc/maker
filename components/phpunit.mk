a22c51ea375e94e167e70d5388ecd6352a164453
SevenPercent Developer <dev7pc@sevenpercent.solutions>
2016-10-04 04:45:35 +0300

    Update PHPUnit style

--- components/phpunit.mk
+++ components/phpunit.mk
@@ -6,8 +6,15 @@ TEST_DIR ?= _tests/
 .PHONY: \
 phpunit-test phpunit-test-pre .phpunit-test phpunit-test-post
 
-phpunit-test: phpunit-test-pre .phpunit-test phpunit-test-post
+phpunit-test: \
+phpunit-test-pre \
+.phpunit-test \
+phpunit-test-post
+
 phpunit-test-pre:: ;
+
 phpunit-test-post:: ;
-.phpunit-test: composer-install
+
+.phpunit-test: \
+composer-install
 	@php vendor/bin/phpunit --bootstrap=vendor/autoload.php '$(TEST_DIR)'
